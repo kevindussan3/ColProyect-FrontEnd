22c3fbe6bf9e273b75733ea83cc839ce9a03d987
kevindussan <kevin.dussan3@gmail.com>
2024-01-17 13:46:31 -0500

    Cambios

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,4 @@
-<template >
+<template>
   <div class="row justify-content-center">
     <div class="col-lg-5 col-md-5">
       <div class="card bg-secondary shadow border-0">
@@ -6,28 +6,46 @@
           <div class="text-center text-muted mb-4">
             <small>Ingresar a la plataforma</small>
           </div>
-          <form @submit.prevent="submitLogin()" @input="clearError" class="signin-form card-body">
-            <div v-for="field in formFields" :key="field.name"
-              class="form-group input-group input-group-alternative mb-3">
+          <form
+            @submit.prevent="submitLogin()"
+            @input="clearError"
+            class="signin-form card-body"
+          >
+            <div
+              v-for="field in formFields"
+              :key="field.name"
+              class="form-group input-group input-group-alternative mb-3"
+            >
               <div class="input-group-prepend">
                 <span class="input-group-text"><i :class="field.iconClass"></i></span>
               </div>
-              <input v-model="usuario[field.name]" :placeholder="field.placeholder" class="form-control"
-                :type="field.type" :required="field.required" />
+              <input
+                v-model="usuario[field.name]"
+                :placeholder="field.placeholder"
+                class="form-control"
+                :type="field.type"
+                :required="field.required"
+              />
             </div>
             <div class="custom-control custom-checkbox custom-control-alternative">
-              <input id="vULJSPm" class="custom-control-input" type="checkbox" />
-              <label for="vULJSPm" class="custom-control-label"><span class="text-muted">Recordar</span></label>
+              <input
+                v-model="usuario.recordar"
+                id="vULJSPm"
+                class="custom-control-input"
+                type="checkbox"
+              />
+              <label for="vULJSPm" class="custom-control-label"
+                ><span class="text-muted">Recordar</span></label
+              >
             </div>
             <div class="text-center">
               <button :disabled="isLoading" type="submit" class="btn btn-primary my-4">
-                {{ isLoading ? 'Ingresando...' : 'Ingresar' }}
+                {{ isLoading ? "Ingresando..." : "Ingresar" }}
               </button>
             </div>
             <div class="text-danger" v-if="error">{{ error }}</div>
             <div class="text-danger" v-if="errorMessage">{{ errorMessage }}</div>
             <div class="text-success" v-if="successMessage">{{ successMessage }}</div>
-
           </form>
         </div>
         <div class="row mt-5">
@@ -35,7 +53,9 @@
             <a href="#" class="text-light"><small>¿Olvidaste tu Contraseña?</small></a>
           </div>
           <div class="col-6 text-right">
-            <router-link to="/register" class="text-light"><small>Crear nueva cuenta</small></router-link>
+            <router-link to="/register" class="text-light"
+              ><small>Crear nueva cuenta</small></router-link
+            >
           </div>
         </div>
       </div>
@@ -52,10 +72,23 @@ export default {
       usuario: {
         email: "",
         password: "",
+        recordar: false,
       },
       formFields: [
-        { name: "email", placeholder: "Correo / Identificación", type: "text", iconClass: "ni ni-email-83", required: true },
-        { name: "password", placeholder: "Contraseña", type: "password", iconClass: "ni ni-lock-circle-open", required: true },
+        {
+          name: "email",
+          placeholder: "Correo / Identificación",
+          type: "text",
+          iconClass: "ni ni-email-83",
+          required: true,
+        },
+        {
+          name: "password",
+          placeholder: "Contraseña",
+          type: "password",
+          iconClass: "ni ni-lock-circle-open",
+          required: true,
+        },
       ],
       error: null,
       isLoading: false,
@@ -70,10 +103,17 @@ export default {
       try {
         this.isLoading = true;
         await this.login(this.usuario);
+        console.log("this.recordar");
+        console.log(this.recordar);
+        if (this.recordar) {
+          localStorage.setItem("usuarioRecordado", JSON.stringify(this.usuario));
+        } else {
+          localStorage.removeItem("usuarioRecordado");
+        }
       } catch (error) {
-        console.log('error XXXX');
         console.log(error);
-        this.error = "Credenciales incorrectas. Por favor, verifica tu correo y contraseña.";
+        this.error =
+          "Credenciales incorrectas. Por favor, verifica tu correo y contraseña.";
       } finally {
         this.isLoading = false;
       }
@@ -81,10 +121,16 @@ export default {
     clearError() {
       this.error = null;
     },
+  },
+  created() {
+    const usuarioRecordado = localStorage.getItem("usuarioRecordado");
+    if (usuarioRecordado) {
+      this.usuario = JSON.parse(usuarioRecordado);
 
-  }
-
-
+      console.log(this.usuario.email);
+      // this.recordar = true;
+    }
+  },
 };
 </script>
 
